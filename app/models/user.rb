f702cec810bc4d101cd6362a142d875b9ed0c60d
thintock <koki.git@alnaturia.com>
2021-02-15 14:51:05 +0000

    favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,12 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :microposts
+  
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
-
+  
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -29,5 +30,21 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+
+  has_many :favorites
+  has_many :favorings, through: :favorites, source: :micropost
+
+  def favorite(other_micropost)
+    self.favorites.find_or_create_by(micropost_id: other_micropost.id)
+  end
+
+  def unfavorite(other_micropost)
+    favorite = self.favorites.find_by(micropost_id: other_micropost.id)
+    favorite.destroy if favorite
+  end
+
+  def favoring?(other_micropost)
+    self.favorings.include?(other_micropost)
   end
 end
